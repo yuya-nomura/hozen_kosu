2d09708826aec3a0dbbbc1e3fbe83c450050ecba
NOMURA YUYA / 野村 侑矢 <yuya.nomura@mail.toyota-shokki.co.jp>
2024-04-03 16:07:53 +0900

    no

--- hozen/settings.py
+++ hozen/settings.py
@@ -103,3 +103,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+CSRF_TRUSTED_ORIGINS = ['https://hozen-kosu.azurewebsites.net']
