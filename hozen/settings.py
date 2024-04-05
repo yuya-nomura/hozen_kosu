4d4d34b11dad4680b4015f630d02dc25be5f09dd
NOMURA YUYA / 野村 侑矢 <yuya.nomura@mail.toyota-shokki.co.jp>
2024-04-05 10:15:03 +0900

    nomu

--- hozen/settings.py
+++ hozen/settings.py
@@ -67,10 +67,10 @@ WSGI_APPLICATION = 'hozen.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
+        'NAME': os.getenv('DB_NAME', ''),
+        'USER': os.getenv('DB_USER', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
         'POST': '5432',
     }
 }
