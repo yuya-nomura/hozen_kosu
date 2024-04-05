dbc6ec7fe2f5a31e0bfdbbb72f0db68406123728
NOMURA YUYA / 野村 侑矢 <yuya.nomura@mail.toyota-shokki.co.jp>
2024-04-05 11:54:34 +0900

    nomu

--- hozen/settings.py
+++ hozen/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER', ''),
         'PASSWORD': os.getenv('DB_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
-        'POST': '5432',
+        'PORT': '5432',
     }
 }
 
