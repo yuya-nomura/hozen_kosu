cca4378b3cfd30746d4fe5e82ea2ffdbb31996db
NOMURA YUYA / 野村 侑矢 <yuya.nomura@mail.toyota-shokki.co.jp>
2024-04-10 16:49:23 +0900

    ライブラリ、CSS設定修正

--- hozen/settings.py
+++ hozen/settings.py
@@ -33,6 +33,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 SESSION_COOKIE_AGE = 315360000
