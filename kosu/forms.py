3c2935ec9e4088feffacdf6ae96b88cbcc6fc6db
NOMURA YUYA / 野村 侑矢 <yuya.nomura@mail.toyota-shokki.co.jp>
2024-06-14 15:03:13 +0900

    工数入力画面フォーム枠線黒に変更

--- kosu/forms.py
+++ kosu/forms.py
@@ -39,12 +39,12 @@ class input_kosuForm(forms.Form):
     ]
 
 
-  tyoku2 = forms.ChoiceField(label = '直', choices = tyoku_list, required = False, widget = forms.Select(attrs ={'class' : 'form-control', 'style' : 'width: 90px;'}))
+  tyoku2 = forms.ChoiceField(label = '直', choices = tyoku_list, required = False, widget = forms.Select(attrs ={'class' : 'form-control custom-border', 'style' : 'width: 90px;'}))
   tomorrow_check = forms.BooleanField(label = '日跨ぎ', required = False, widget = forms.CheckboxInput(attrs = {'class' : 'form-check-input'}))
-  kosu_def_list = forms.ChoiceField(label = '工数区分', required = False, widget = forms.Select(attrs = {'class' : 'form-control', 'style' : 'width: 210px;'}))
-  work_detail = forms.CharField(label = '作業詳細', required = False, widget = forms.TextInput(attrs = {'class' : 'form-control', 'placeholder' : '未入力可 メモで使用', 'style' : 'width: 210px;'}))
-  over_work = forms.IntegerField(label = '残業', widget=forms.NumberInput(attrs = {'class' : 'form-control', 'min' : 0, 'style' : 'width: 80px;'}), required = False)
-  work = forms.ChoiceField(label = '勤務', choices = employment_list, required = False, widget = forms.Select(attrs = {'class' : 'form-control', 'style' : 'width: 90px;'}))
+  kosu_def_list = forms.ChoiceField(label = '工数区分', required = False, widget = forms.Select(attrs = {'class' : 'form-control custom-border', 'style' : 'width: 210px;'}))
+  work_detail = forms.CharField(label = '作業詳細', required = False, widget = forms.TextInput(attrs = {'class' : 'form-control custom-border', 'placeholder' : '未入力可 メモで使用', 'style' : 'width: 210px;'}))
+  over_work = forms.IntegerField(label = '残業', widget=forms.NumberInput(attrs = {'class' : 'form-control custom-border', 'min' : 0, 'style' : 'width: 80px;'}), required = False)
+  work = forms.ChoiceField(label = '勤務', choices = employment_list, required = False, widget = forms.Select(attrs = {'class' : 'form-control custom-border', 'style' : 'width: 90px;'}))
   break_change = forms.BooleanField(label = '休憩変更', required = False, widget = forms.CheckboxInput(attrs = {'class' : 'form-check-input'}))
 
 
