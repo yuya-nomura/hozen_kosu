e7b25bc9f53b254313336f4583dda52a24449442
NOMURA YUYA / 野村 侑矢 <yuya.nomura@mail.toyota-shokki.co.jp>
2024-12-10 16:53:10 +0900

    関数まとめ

--- kosu/utils.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-def round_time(dt=None, round_to=5):
-    """指定された分単位で時間を丸める関数"""
-    if dt is None:
-        dt = datetime.datetime.now().time()
-    minute = (dt.minute // round_to) * round_to
-    return dt.replace(minute=minute, second=0, microsecond=0)
